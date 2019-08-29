f92f18cf36738c7c6629f913eb976ea35ed0efe8
Alexneksikan <alexneksikan@rambler.ru>
2019-08-29 18:45:12 +0300

    Работа с MySQL из PHP-сценариев

--- index.php
+++ index.php
@@ -2,57 +2,13 @@
 
 require_once('helpers.php');
 
+// Подключение БД
+$con = mysqli_connect("localhost", "root", "", "yeticave");
+mysqli_set_charset($con, "utf8");
+
 $is_auth = rand(0, 1);
 
 $user_name = 'Александр'; // укажите здесь ваше имя
-
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-
-$lots_info = [
-
-            [
-                'name' => '2014 Rossignol District Snowboard',
-                'category' => 'Доски и лыжи',
-                'price' => 10999,
-                'url' => 'img/lot-1.jpg',
-                'finish_date' => '2019-09-01'
-            ],
-            [
-                'name' => 'DC Ply Mens 2016/2017 Snowboard',
-                'category' => 'Доски и лыжи',
-                'price' => 159999,
-                'url' => 'img/lot-2.jpg',
-                'finish_date' => '2019-08-23'
-            ],
-            [
-                'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-                'category' => 'Крепления',
-                'price' => 8000,
-                'url' => 'img/lot-3.jpg',
-                'finish_date' => '2019-09-02'
-            ],
-            [
-                'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-                'category' => 'Ботинки',
-                'price' => 10999,
-                'url' => 'img/lot-4.jpg',
-                'finish_date' => '2019-08-30'
-            ],
-            [
-                'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-                'category' => 'Одежда',
-                'price' => 7500,
-                'url' => 'img/lot-5.jpg',
-                'finish_date' => '2019-11-17'
-            ],
-            [
-                'name' => 'Маска Oakley Canopy',
-                'category' => 'Разное',
-                'price' => 5400,
-                'url' => 'img/lot-6.jpg',
-                'finish_date' => '2019-10-11'
-            ]
-];
 
 function formatPrice($lot_price)
 {
@@ -81,6 +37,14 @@ function get_dt_range($f_date)
 }
 
 
+$sql = "SELECT l.name, l.start_price, l.image, l.finish_price, l.finish_date, c.name category, l.image FROM lots l, categories c WHERE l.category=c.id AND l.winner IS NULL ORDER BY start_date DESC";
+$lots_info = mysqli_query($con, $sql);
+
+$sql = "SELECT name, symbol_code FROM categories;";
+$categories = mysqli_query($con, $sql);
+
+
+
 $page_content = include_template('main.php', [
     'categories' => $categories,
     'lots_info' => $lots_info
@@ -90,7 +54,7 @@ $layout_content = include_template('layout.php', [
     'content' => $page_content,
     'title' => $title,
     'user_name' => $user_name,
-    'is_auth' => $is_auth,   
+    'is_auth' => $is_auth,
     'categories' => $categories
 ]);
 
